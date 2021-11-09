fda751168b23f523024f3adff93aea4bc50164a5
Carolina Romanos <carolina.romanos.roca@gmail.com>
2021-11-09 16:58:56 +0100

    express app

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, "public")))
+
+app.all("/home", (request, response)=>{
+    response.sendFile(path.join(__dirname, "views", "home.html"))
+
+})
+app.all("/about", (request, response)=>{
+    response.sendFile(path.join(__dirname, "views", "about.html"))
+    
+})
+
+app.all("/works", (request,response)=>{
+    response.sendFile(path.join(__dirname, "views", "works.html"))
+})
+app.listen(3000, ()=>{
+    console.log("App running")
+})
